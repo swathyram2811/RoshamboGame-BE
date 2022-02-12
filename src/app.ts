10604f2ea6e5be5987546cbca677e69ade0c3fdf
swathyram2811 <swathyram2811@gmail.com>
2022-02-12 12:35:13 +0100

    feat(service): proj init setup

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import * as http from "http";
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import { config } from "./config";
+import { Routes } from "./common/common.routes";
+
+const app: express.Application = express();
+const server: http.Server = http.createServer(app);
+const routes: Routes[] = [];
+
+app.use(express.json());
+
+const health = `Express running at http://localhost:${config.SERVER_PORT}`;
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.status(200).send(health);
+});
+
+server.listen(config.SERVER_PORT, () => {
+  routes.forEach((route: Routes) => {
+    route.configureRoutes();
+    console.log(`Configured routes ${route.getName()}`);
+  });
+  console.log(health);
+});
+
+export default server;
---
+++ src/config/index.ts
@@ -0,0 +1,3 @@
+export const config = {
+  SERVER_PORT: process.env.SERVER_PORT,
+};
